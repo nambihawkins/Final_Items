5a230dcd00d6faaae4a69590003cf7acd776536d
NomNom <nambihawkins@Nambis-MacBook-Pro.local>
2018-05-28 19:06:26 -0400

    Change list type to grocery list, and change variable/class/id/title names to match

--- PortfolioPipeline/Updated/ProgrammingForWebApps2_Project4/js/script.js
+++ PortfolioPipeline/Updated/ProgrammingForWebApps2_Project4/js/script.js
@@ -2,13 +2,13 @@
  * Created by Nambi on 6/24/16.
  */
 $(function() {
-    $( "#due-date" ).datepicker();
-    $( "#add-todo" ).button({
+
+    $( "#add-item" ).button({
         icons: {primary: "url-icon-circle-plus"}
     }).click(function(){
-        $("#new-todo").dialog("open");
+        $("#new-item").dialog("open");
     });
-    $( "#new-todo" ).dialog({
+    $( "#new-item" ).dialog({
         width: 350,
         height: 300,
         modal:true,
@@ -22,18 +22,18 @@ $(function() {
             duration: 1000
         },
         close: function () {
-            $("#new-todo, input").val("");
+            $("#new-item, input").val("");
         },
         buttons: {
-            "Add task" : function() {
-                var taskName = $('#task').val(),
-                    dueDate = $('#due-date').val(),
+            "Add item" : function() {
+                var itemName = $('#item').val(),
+                    quantity = $('#quantity').val(),
                     beginLi = "<li><span class='done'></span><span class='delete'>x</span>",
-                    taskLi = "<span class='task'>" + taskName + "</span>",
-                    dateLi = "<span class='due-date'>" + dueDate + "</span>",
+                    itemLi = "<span class='item'>" + itemName + "</span>",
+                    quantityLi = "<span class='quantity'>" + quantity + "</span>",
                     endLi = "</li>",
-                    td = $('#todo-list');
-                td.prepend(beginLi + taskLi + dateLi + endLi);
+                    td = $('#items-list');
+                td.prepend(beginLi + itemLi + quantityLi + endLi);
                 td.hide().slideDown(250).find('li:first').animate({'background-color': 'rgb(255,255,204'}, 250).animate({'background-color': 'white'
                 }, 750).animate();
                 $(this).dialog('close');
@@ -43,9 +43,9 @@ $(function() {
             }
         }
     });
-    $('#todo-list').on('click', '.done', function () {
-        var taskItem = $(this).parent('li');
-        taskItem.slideUp(250, function () {
+    $('#items-list').on('click', '.done', function () {
+        var itemItem = $(this).parent('li');
+        itemItem.slideUp(250, function () {
             var $this = $(this);
             $this.detach();
             $('completed-list').prepend($this);
